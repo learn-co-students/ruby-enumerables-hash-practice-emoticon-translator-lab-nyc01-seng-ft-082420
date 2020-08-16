a20339768a362bb909c8e0bf8d5a9af3df7f932e
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-16 20:12:14 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,20 +1,33 @@
 require "yaml"
 require "pry"
 
-def load_library(path)
-  emo_hash = YAML.load_file(path)
+# def load_library(path)
+#   emo_hash = YAML.load_file(path)
   
-  emoticons = emo_hash.each_with_object({}) do |(key, value), emoticons|
-    #grabbing roman names with key, both emoticons stored in value
-    if !emoticons[key]
-      emoticons[key] = {
-        english: value[0],
-        japanese: value[1]
-      }
-    end
+#   emoticons = emo_hash.each_with_object({}) do |(key, value), emoticons|
+#     #grabbing roman names with key, both emoticons are going to be stored in value
 
+#     if !emoticons[key]
+#       emoticons[key] = {
+#         english: value[0],
+#         japanese: value[1]
+#       }
+#     end
+
+#   end
+#   binding.pry
+# end
+
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  emoticon_hash = {}
+  
+  emoticons.each do |name, value|
+    # binding.pry
+    emoticon_hash[name] = {:english => value[0], :japanese => value[1]}
   end
   
+  emoticon_hash
 end
 
 
